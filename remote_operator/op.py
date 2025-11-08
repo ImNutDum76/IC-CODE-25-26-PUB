d9c928466d2a8dd2f2e1aefae4975e03724a1a63
Nick <tntnick07@gmail.com>
2025-11-07 20:35:41 -0500

    followed tutorial

--- remote_operator/op.py
+++ remote_operator/op.py
@@ -4,11 +4,12 @@ import subprocess
 import sys
 import time
 import threading
+import keyboard
 import pygame
 import os
 
 # ============ USER CONFIG ============
-PI_IP = ""    # Your Pi's IP on IC2026 network
+PI_IP = "192.168.50.204"    # Your Pi's IP on IC2026 network
 PI_PORT = 5005
 
 AUTO_LAUNCH_GSTREAMER = True
@@ -26,16 +27,57 @@ GST_RECEIVER_CMD_AVD = (
 SEND_HZ = 30
 
 ### Sockets
-
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # create a UPD/datagram socket
+addr = (PI_IP, PI_PORT)
 ### Video Stream
+def open_stream():
+    global gst_proc
 
+    if AUTO_LAUNCH_GSTREAMER:
+        try:
+            gst_proc = subprocess.Popen(GST_RECEIVER_CMD, shell=True) # run the command we wrote in the shell
+            print("[Video] GStreamer started")
+        except Exception as e:
+            print(f"[Video] Failed: {e}")
 ### Clean Up
+def cleanup():
+    if gst_proc and gst_proc.poll() is None:
+        gst_proc.terminate() # terminate the subprocess
 
+    sock.close() # stop the socket from running
+
+input_thread = None
 ### Input Loop
+def input_loop():
+    vx = 0
+    vy = 0
+    if keyboard.is_pressed("w"):
+        vy = 1
+    elif keyboard.is_pressed("s"):
+        vy = 1
 
+    if keyboard.is_pressed("a"):
+        vx += 1
+    if keyboard.is_pressed("d"):
+        vx -= 1
+
+    rot = 0
+    if keyboard.is_pressed("right"):
+        rot = 1
+    elif keyboard.is_pressed("left"):
+        rot = -1
+    payload = {
+        "vx": float(vx),
+        "vy": float(vy),
+        "rot": float(rot)
+        }
+    input_thread = threading.Thread(target=input_loop, daemon=True)
+
+#input_thread = threading.Thread(target=input_loop, daemon=True)
 ### Main Loop
 def main():
     ### Open Stream
+    open_stream()
 
     pygame.init()
     screen = pygame.display.set_mode((1280, 720))
@@ -43,12 +85,13 @@ def main():
     running = True
 
     ### Start Input Thread
+    input_thread.start()
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 ### Start Cleanup
-
+                cleanup()
                 running = False
                 pygame.quit()
             screen.fill("black")
--- robot/pi.py
+++ robot/pi.py
@@ -8,51 +8,128 @@ import sys
 import time
 import threading
 import pigpio
+import socket
 
 from readonly import RobotBase, MOTORS
 
-OPERATOR_IP = "" # your laptop/pc ip address on IC2026 Network
+OPERATOR_IP = "192.168.50.53" # your laptop/pc ip address on IC2026 Network
 OPERATOR_PORT = 5600 # the port for video streaming 
-TEAM_ID = -1 # Your team ID
+TEAM_ID = 19 # Your team ID
 
-PI_IP = "" # Your pi IP
+PI_IP = "192.168.50.204" # Your pi IP
 PI_PORT = 5005  # 
 
 MIN_DUTY_FLOOR = 30
 PURE_DC_THRESHOLD = 80
 
 ### Bind Socket
-
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((PI_IP, PI_PORT))
 ### Input Receiving Loop
+inputQ = []
+def get_input():
+    while True:
+        try:
+            data, addr = sock.recvfrom(1024) 
+            msg = json.loads(data.decode('utf-8'))
+            inputQ.append(msg)
+            print(f"[Received from {addr}] {msg}")
+        except Exception as e:
+            print("[Receiver Error]", e)
 
 class Robot(RobotBase):
     def __init__(self, team_id):
         super().__init__(team_id)
         ### Initialization/Start Up
-        pass
+        self.stream_proc = None
 
         ### Socket Receive Thread
+        self.input_thread = threading.Thread(target=get_input, daemon=True)
+        self.input_thread.start()
+
+    def mecanum_drive(self):
+        if len(inputQ) > 0:
+            inputJSON = inputQ.pop(0)
+            vx = inputJSON["vx"]
+            vy = inputJSON["vy"]
+            rot = inputJSON["rot"]
+
+            fl = vy + vx + rot
+            fr = -vy + vx - rot
+            bl = -vy + vx + rot
+            br = vy + vx - rot
+           
+            scale = max(1.0, abs(fl), abs(fr), abs(bl), abs(br))
+            fl /= scale; fr /= scale; rl /= scale; rr /= scale # normalize each speed
+
+            self.set_motor("FL", fl)
+            self.set_motor("BL", bl)
+            self.set_motor("FR", fr)
+            self.set_motor("BR", br)
+
 
     def run(self):
         try:
             while True:
-                pass
+                self.mecanum_drive()
+        except KeyboardInterrupt:
+            sys.stderr.write("\n[Shutdown] Keyboard interrupt\n")
         except Exception as e:
-            pass
+            sys.stderr.write(f"[Runtime Error] {e}\n")
         finally:
             self.cleanup()
 
     def stream(self):
-        pass
+        cmd = (
+            f"rpicam-vid -t 0 --width 1280 --height 720 --framerate 30 "
+            f"--codec h264 --bitrate 4000000 --profile baseline --intra 30 --inline "
+            f"--nopreview -o - | "
+            f"gst-launch-1.0 -v fdsrc ! h264parse ! "
+            f"rtph264pay config-interval=1 pt=96 ! "
+            f"udpsink host={OPERATOR_IP} port={OPERATOR_PORT} sync=false async=false"
+        )
+   
+        self.stream_proc = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        print(f"[Video] Stream started -> {OPERATOR_IP}:{OPERATOR_PORT}")
 
-    def set_motor(self):
-        pass
+def set_motor(self,name, norm):
+    """Apply motor control with PWM"""
+    norm = math.clamp(norm, -1.0, 1.0)
+    pins = MOTORS[name]
+   
+    if abs(norm) < 1e-3:
+        self.pi.set_PWM_dutycycle(pins["EN"], 0)
+        self.pi.write(pins["IN1"], 0)
+        self.pi.write(pins["IN2"], 0)
+        return
+   
+    forward = norm > 0
+    self.pi.write(pins["IN1"], 1 if forward else 0)
+    self.pi.write(pins["IN2"], 0 if forward else 1)
+   
+    pct = int(abs(norm) * 100)
+    if pct >= PURE_DC_THRESHOLD:
+        self.pi.write(pins["EN"], 1)
+    else:
+        pct = max(MIN_DUTY_FLOOR, pct)
+        duty = pct * 255 // 100
+        self.pi.set_PWM_dutycycle(pins["EN"], duty)
 
     def cleanup(self):
-        pass
+        if self.stream_proc and self.stream_proc.poll() is None:
+            try:
+                os.killpg(os.getpgid(self.stream_proc.pid), signal.SIGTERM)
+                self.stream_proc.wait(timeout=2)
+            except:
+                pass
+            print("[Video] Stream stopped")
+        self.stream_proc = None
+
+        for receiver in self.ir_receivers:
+            receiver.cleanup()
 
 if __name__ == "__main__":
-    team_id = 0  # your team id here
+    team_id = TEAM_ID  # your team id here
 
     robot = Robot(team_id)
     try:
